f22243f5181a90ff4f5ec3444a5d2f5f6f496967
manish.thakur23 <manish.thakur23@outlook.com>
2021-05-29 15:48:49 +0530

    Added code to find the sum of numbers in array which is equal to given number

--- src/Array/FindPairsOfSum.java
+++ src/Array/FindPairsOfSum.java
@@ -5,24 +5,37 @@ import java.util.HashMap;
 public class FindPairsOfSum {
 
 	public static int getPairSum(int [] a, int k) {
-		int count = 0;
+		int twice_count = 0;
 		
 		HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+		for(int i = 0 ; i<a.length; i++) {
+			if(!m.containsKey(a[i])) {
+				m.put(a[i], 0);
+			}
+			m.put(a[i], m.get(a[i])+1);
+		}
 		
-		
-		for(int i =0 ; i<a.length; i ++) {
-			int x = k-a[i];
+		for(int i = 0 ; i < a.length ; i++) {
+			if(m.get(k - a[i]) != null) {
+				twice_count += m.get(k-a[i]);
+			}
 			
-			if(!m.containsKey(x)) {
-				//m.put(a[i];
-				
-				
+			if(k-a[i] == a[i]) {
+				twice_count--;
 			}
 		}
-		return count ++;
+			
+		
+		return twice_count/2 ;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int arr[] = new int[] { 1, 5, 7, -1, 5 };
+		int sum = 6;
+        System.out.println(
+            "Count of pairs is "
+            + getPairSum(arr, sum));
 
 	}
 
